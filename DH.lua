85788591585f4ed9c5304baa75eed67699f5fe74
Spooky <96256529+SrGask@users.noreply.github.com>
2021-12-24 15:14:01 -0300

    Update DH.lua

--- DH.lua
+++ DH.lua
@@ -6,7 +6,9 @@ local serv = win:Server("Scripts", "http://www.roblox.com/asset/?id=6031280882")
 
 local btns = serv:Channel("Swag Mode")
 
-local win = DiscordLib:Notification("Notificacion", "Bienvenido a [G.A] Scripts Library!", "Que pro B)")
+local win = DiscordLib:Notification("Notificacion", "Bienvenido a [G.A] Scripts Hub!", "Que pro B)")
+
+--------------------------------------------------------------------------------------------------------------------------------------
 
   game.StarterGui:SetCore("SendNotification", {
     Title = 'Bienvenido';
@@ -21,7 +23,11 @@ local win = DiscordLib:Notification("Notificacion", "Bienvenido a [G.A] Scripts 
     Duration = 10;
   })
 
+--------------------------------------------------------------------------------------------------------------------------------------
+
 loadstring(game:HttpGet("https://pastebin.com/raw/nzXicwc1", true))() -- Chatspy
+
+--------------------------------------------------------------------------------------------------------------------------------------
 
 btns:Button("Swag Mode", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SwagModeV002'))()
@@ -34,6 +40,9 @@ btns:Button("Swag Mode OLD", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/OldSwagmode'))()
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
 
 local btns = serv:Channel("Faded")
 
@@ -49,6 +58,9 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/NighterEpic/Old-Faded
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
 local btns = serv:Channel("Ray Codex")
 
 btns:Button("Ray Codex", function()
@@ -56,12 +68,18 @@ loadstring(game:GetObjects("rbxassetid://5812737894")[1].Source)()
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
 local btns = serv:Channel("RayX")
 
 btns:Button("RayX", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SpaceYes/Lua/Main/DaHood.Lua'))() 
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
 
 local btns = serv:Channel("$moke")
 
@@ -86,6 +104,9 @@ btns:Seperator()
 btns:Label("                                   [Esta medio bug]", function()
 end)
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
 local btns = serv:Channel("V.G Hub")
 
 btns:Button("V.G Hub", function()
@@ -99,6 +120,9 @@ btns:Button("Trust Boy", function()
 loadstring(game:HttpGet('https://pastebin.com/raw/XXAWmifh'))()
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
 
 local btns = serv:Channel("Destiny")
 
@@ -115,6 +139,9 @@ end)
 btns:Label("                             Link:roblox.com/7658734", function()
 end)
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
 local btns = serv:Channel("Arctic")
 
 btns:Button("Arctic", function()
@@ -130,6 +157,9 @@ end)
 btns:Label("                                  Se copia al ejecutar", function()
 end)
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
 local btns = serv:Channel("Dark.exe")
 
 btns:Button("Dark.exe", function()
@@ -142,6 +172,9 @@ btns:Seperator()
 btns:Button("Comando", function()
 DiscordLib:Notification("Notificacion", ":dark.exe", "ez")
 end)
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
 
 local btns = serv:Channel("Dawny")
 
@@ -169,12 +202,18 @@ end)
 btns:Label("                                  Se copia al ejecutar", function()
 end)
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
 local btns = serv:Channel("BoxWare")
 
 btns:Button("BoxWare", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/crypthook/some-shit-script-crack/main/boxware"))()
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
 
 local btns = serv:Channel("HoodShark")
 
@@ -183,6 +222,9 @@ loadstring(game:HttpGet("https://pastebin.com/raw/7cGyt1mi"))()
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
 local btns = serv:Channel("DemonWare")
 
 btns:Button("DemonWare", function()
@@ -190,12 +232,18 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/JacobCTT/dwISWnextUpd
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
 local btns = serv:Channel("NovaGUI")
 
 btns:Button("NovaGUI", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/IcxyLol/Nova-Gui-New/main/README.md", true))()
 DiscordLib:Notification("Notificacion", "Ejecutado!", "ez")
 end)
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
 
 local btns = serv:Channel("Krypt")
 
@@ -210,28 +258,8 @@ btns:Button("Krypt Key", function()
 DiscordLib:Notification("Notificacion", "Key: kryptrevival", "ez")
 end)
 
---------------------------------------------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------------------------------------------
-
-wait(5)
-if ModCheck[player.UserId] then
-    IsMod = true
-    DisableMod = true
-    announce('Se te han dado rango Mod', 'Eres Inmune a todo Comando de Mod', 10)
-elseif table.find(swagmins, player.UserId) then
-    announce('Eres acmin', 'hi :3', 10)
-    IsMod = false
-    DisableMod = false
-else
-    IsMod = false
-    DisableMod = false
-    announce('Comandos Cargados', 'Que pro', 10)
-end
-end)
-if err then print(err) end
-
 -------------------------------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------------
 
 local swagmins = {
   3098428016, -- Unbotdesofia2
@@ -364,5 +392,21 @@ for i,v in pairs(game:GetService('Players'):GetChildren()) do
     end
 end
 
-------------------------------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+
+wait(5)
+if ModCheck[player.UserId] then
+    IsMod = true
+    DisableMod = true
+    announce('Se te han dado rango Mod', 'Eres Inmune a todo Comando de Mod', 10)
+elseif table.find(swagmins, player.UserId) then
+    announce('Eres acmin', 'hi :3', 10)
+    IsMod = false
+    DisableMod = false
+else
+    IsMod = false
+    DisableMod = false
+    announce('Comandos Cargados', 'Que pro', 10)
+end
+if err then print(err) end
